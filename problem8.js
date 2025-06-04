2a69fbda200df91a518d3e3d32e0badd5cca3edb
alamshuvo <alamshuvozeny019@gmail.com>
2025-06-04 11:58:26 +0600

    problem 8 added

---
+++ problem8.js
@@ -0,0 +1,7 @@
+// -Create an array of numbers with some duplicate values. Write a function to filter out the duplicate values and return a new array with only unique numbers. Print the result.
+const removeDuplicates = (numbers)=>{
+    const uniqueNumbers = numbers.filter((num,index)=> numbers.indexOf(num) === index);
+    return uniqueNumbers;
+}
+//  const num = [1,2,2,5,6,3,8,1,2]
+// console.log(removeDuplicates(num));
