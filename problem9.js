c431dfac3b3be15c636c2f29b24d883990d557ac
alamshuvo <alamshuvozeny019@gmail.com>
2025-07-03 21:52:43 +0600

    some code added

---
+++ problem9.js
@@ -0,0 +1,24 @@
+// find the second largest number in array without using any built in function 
+
+
+const findSecondLargestNumber = (arr)=>{
+// console.log(arr);
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    // console.log(element);
+    let firstElement = element;
+    let secondLargest;
+    if (element>largest) {
+        secondLargest = largest;
+        console.log(secondLargest);
+        console.log("bitore duksi");
+    }
+    console.log("ekhane");
+   
+}
+
+}
+
+const arr = [12,4,13];
+findSecondLargestNumber(arr);
+// some code addded 
